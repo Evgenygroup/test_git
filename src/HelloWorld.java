26443332edc307fb8d78cef8682c3f77e0c0146b
Evgenygroup <mailfrom61@gmail.com>
2025-01-13 15:05:52 +0100

    adds bad good code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world !");
         System.out.println("commit3");
+        System.out.println("плохой");
+        System.out.println("хороший");
     }
 }
