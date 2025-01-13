8f1012a483f8f140e90f4245f38a14a023377537
Evgenygroup <mailfrom61@gmail.com>
2025-01-13 12:50:13 +0100

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,15 @@
 public class HelloWorld {
 
 
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world !");
-    }
+
+    System.out.println("Hello world !");
+    System.out.println("Print commit 2 !");
+
+
+}
+
+
 }
